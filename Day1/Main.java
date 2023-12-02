3d012cdfb7919fd92b99e5d8ba2e4f511f3b1853
Donny Hyon <dhyon333@hotmail.co.uk>
2023-12-02 12:28:34 +0000

    WIP- issue when parsing wordy numbers

--- Day1/Main.java
+++ Day1/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class Main{
     List<String> parsedData = new ArrayList<String>();
+    List<String> withoutWordyNumbers = new ArrayList<String>();
     List<String> characterlessData = new ArrayList<String>();
     List<String> removedSingles = new ArrayList<String>();
     List<String> removedLongerNumbers = new ArrayList<String>();
@@ -14,14 +15,16 @@ public class Main{
     public static void main(String[] args) {
         Main main = new Main();
         main.readDataFromFile();
+        main.parseWordedNumbers();
         main.removeLetters();
         main.removeSingleCharacters();
         main.removeDoubleCharacters();
         main.calculateTotal();
         System.out.println(main.sum);
+            
 
 
-    }     
+    }
     
     public void readDataFromFile() {
         try {
@@ -38,8 +41,24 @@ public class Main{
           }
     }
 
+    public void parseWordedNumbers(){
+        for (int i = 0 ; i < parsedData.size(); i++){
+            String newString = parsedData.get(i);
+            newString = newString.replaceAll("one", "1");
+            newString = newString.replaceAll("two", "2");
+            newString = newString.replaceAll("three", "3");
+            newString = newString.replaceAll("four", "4");
+            newString = newString.replaceAll("five", "5");
+            newString = newString.replaceAll("six", "6");
+            newString = newString.replaceAll("seven", "7");
+            newString = newString.replaceAll("eight", "8");
+            newString = newString.replaceAll("nine", "9");
+            withoutWordyNumbers.add(newString);
+        }
+    }
+
     public void removeLetters(){
-        for (String singleCoordinate : parsedData) {
+        for (String singleCoordinate : withoutWordyNumbers) {
             characterlessData.add(singleCoordinate.replaceAll("[a-zA-Z]+", ""));
         }
     }
