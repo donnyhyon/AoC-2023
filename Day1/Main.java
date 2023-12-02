5f8b8b01dea5f096f8b4247ae7543b08de81b32f
Donny Hyon <dhyon333@hotmail.co.uk>
2023-12-02 11:27:55 +0000

    handled single and longer strings

--- Day1/Main.java
+++ Day1/Main.java
@@ -7,11 +7,15 @@ import java.util.Scanner;
 public class Main{
     static List<String> parsedData = new ArrayList<String>();
     static List<String> characterlessData = new ArrayList<String>();
+    static List<String> removedSingles = new ArrayList<String>();
+    static List<String> removedLongerNumbers = new ArrayList<String>();
 
     public static void main(String[] args) {
         reader();
         alphabetRemover();
-        System.out.println(characterlessData);
+        singleHandler();
+        doubleHandler();
+        System.out.println(removedLongerNumbers);
 
 
 
@@ -23,7 +27,6 @@ public class Main{
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
               String data = myReader.nextLine();
-              System.out.println(data);
               parsedData.add(data);
             }
             myReader.close();
@@ -36,6 +39,29 @@ public class Main{
     public static void alphabetRemover(){
         for (String singleCoordinate : parsedData) {
             characterlessData.add(singleCoordinate.replaceAll("[a-zA-Z]+", ""));
+        }
+    }
+
+    public static void singleHandler() {
+        for (String string : characterlessData) {
+            if(string.length() == 1) {
+                removedSingles.add(string + string);
+            } else {
+                removedSingles.add(string);
+            }
+        }
+    }
+
+        public static void doubleHandler() {
+        for (String string : removedSingles) {
+            if(string.length() > 2 ) {
+                char firstChar = string.charAt(0);
+                char lastChar = string.charAt(string.length() -1);
+                String shortenedSrtring = Character.toString(firstChar) + Character.toString(lastChar);
+                removedLongerNumbers.add(shortenedSrtring);
+            } else {
+                removedLongerNumbers.add(string);
+            }
         }
     }
 
