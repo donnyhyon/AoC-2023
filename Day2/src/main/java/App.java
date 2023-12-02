74125140a8bca7208c914d3050051ccec789cedf
Donny Hyon <dhyon333@hotmail.co.uk>
2023-12-02 23:17:15 +0000

    WIP- fix indexing through RGB array

--- Day2/src/main/java/App.java
+++ Day2/src/main/java/App.java
@@ -2,25 +2,38 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Day2 AoC Challenge" );
     }
 
     public String[] colonSplit(String gameData){
-        String[] splitData = new String[2];
-        splitData = gameData.split(": ");
-        return splitData;
+        String[] colonSplitData = new String[2];
+        colonSplitData = gameData.split(": ");
+        return colonSplitData;
     }
 
     public String[] semiColonSplit(String gameData){
-        String[] splitData = new String[2];
-        splitData = gameData.split("; ");
-        return splitData;
+        String[] semiColonSplitData = new String[2];
+        semiColonSplitData = gameData.split("; ");
+        return semiColonSplitData;
     }
 
     public String[] commaSplit(String gameData){
-        String[] splitData = new String[2];
-        splitData = gameData.split(", ");
-        return splitData;
+        String[] commaSplitData = new String[2];
+        commaSplitData = gameData.split(", ");
+        return commaSplitData;
+    }
+
+    public String[] orderToRGB(String[] unOrderedRGB){
+        String[] rGBData = new String[2];
+        int redIndex = unOrderedRGB.indexOf(".*red.*");
+        int greenIndex = unOrderedRGB.indexOf(".*green.*");
+        int blueIndex = unOrderedRGB.indexOf(".*blue.*");
+        rGBData[0] = (redIndex != -1)? unOrderedRGB[redIndex] : "0 red";
+        rGBData[1] = (greenIndex != -1)? unOrderedRGB[greenIndex] : "0 green";
+        rGBData[2] = (blueIndex != -1)? unOrderedRGB[blueIndex] : "0 blue";
+
+        return rGBData;
+
     }
     
 }
