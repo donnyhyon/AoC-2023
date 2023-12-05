dd93a10111889011823e388ad451406bbde85642
Donny Hyon <dhyon333@hotmail.co.uk>
2023-12-05 15:46:51 +0000

    init commit day3

---
+++ day3/src/main/java/day3/App.java
@@ -0,0 +1,9 @@
+package day3.src.main.java.day3;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ day3/src/test/java/day3/AppTest.java
@@ -0,0 +1,14 @@
+package day3.src.test.java.day3;
+
+import org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
