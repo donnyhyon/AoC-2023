bc85506317fe610f51e75850629f4b7608dbcdb4
Donny Hyon <105787366+donnyhyon@users.noreply.github.com>
2023-12-05 22:00:59 +0000

    parsing file

--- day3/my-app/src/main/java/com/mycompany/app/App.java
+++ day3/my-app/src/main/java/com/mycompany/app/App.java
@@ -1,9 +1,24 @@
 package com.mycompany.app;
 
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello day3" );
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("data2.txt"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
