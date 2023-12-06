39ca971ab75a877f8a7211ede4a124bc9a031942
Donny Hyon <dhyon333@hotmail.co.uk>
2023-12-06 12:44:10 +0000

    green file parsed

--- day3/my-app/src/main/java/com/mycompany/app/App.java
+++ day3/my-app/src/main/java/com/mycompany/app/App.java
@@ -1,24 +1,32 @@
 package com.mycompany.app;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( )
     {
         System.out.println( "Hello day3" );
+    }
+
+    public List<String> readDataFile(String filepath){
+        List<String> parsedLines = new ArrayList<>();
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("data2.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader(filepath));
             String line;
             while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+
+                parsedLines.add(line);
             }
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return parsedLines;
     }
 }
 
--- day3/my-app/src/test/java/com/mycompany/app/AppTest.java
+++ day3/my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -2,13 +2,26 @@ package com.mycompany.app;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
 import org.junit.Test;
 
 public class AppTest 
 {
     @Test
-    public void shouldAnswerWithTrue()
+    public void shouldParseFile()
     {
-        assertTrue( true );
+        App game = new App();
+        List<String> file = game.readDataFile("data2.txt");
+        // System.out.println(file);
     }
+
+    
+    // parse data table into x and y co-ords
+    // iterate through each line. find numbers within that line.
+    //  for each number, get the first and last index of that number == grid
+    //  expand grid out by 1 square in every direction...
+    // create a search function to check for any unique characters within a given grid
+
+
 }
