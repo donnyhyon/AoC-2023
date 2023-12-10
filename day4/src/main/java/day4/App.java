e0b15ee1d3d5574fff744920ecf11970afd01cd5
donnyhyon <dhyon333@hotmail.co.uk>
2023-12-10 20:32:41 +0000

    failing split file test

--- day4/src/main/java/day4/App.java
+++ day4/src/main/java/day4/App.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 
 public class App 
 {   
-    static String[] rawData;
+    static ArrayList<String> rawData = new ArrayList<>();
 
     public static class Card
     {
@@ -24,31 +24,26 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        readDataFile("path_to_your_file.txt");
-        for(String line : rawData) {
-            System.out.println(line);
-        }
     }
 
-    public static void readDataFile(String filePath) {
-        List<String> lines = new ArrayList<>();
+    public void readDataFile(String filePath) {
         try {
             FileReader reader = new FileReader(filePath);
             BufferedReader bufferedReader = new BufferedReader(reader);
 
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
+                rawData.add(line);
             }
             bufferedReader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        rawData = lines.toArray(new String[0]);
     }
 
     public Card splitScoreCard(String aScoreCard) {
         Card card = new Card();
+
 
 
         return card;
--- day4/src/test/java/day4/AppTest.java
+++ day4/src/test/java/day4/AppTest.java
@@ -22,12 +22,12 @@ public class AppTest
         App game = new App();
         String input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53";
         Card card = new Card();
+        game.readDataFile("data2.txt");
         card = game.splitScoreCard(input);
         int[] winningNumbers = card.winningNumbers;
         int[] usersNumbers = card.usersNumbers;
         assertEquals(new int[]{17,41,48,83,86}, winningNumbers);
         assertEquals(new int[]{6,9,17,31,48,53,83,86}, usersNumbers);
-
     }
 
     
