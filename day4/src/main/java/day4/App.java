2737d64855e27b82821096215347610dca9849cc
donnyhyon <dhyon333@hotmail.co.uk>
2023-12-13 17:11:33 +0000

    green check array

--- day4/src/main/java/day4/App.java
+++ day4/src/main/java/day4/App.java
@@ -80,4 +80,17 @@ public class App
         
     }
 
+    public int checkWinningNumbers(int[] array1, int[] array2) {
+        int matches = 0;
+        for (int i = 0 ; i < array1.length ; i ++ ){
+            for (int j = 0; j < array2.length; j ++){
+                if (array1[i] == array2[j]){
+                    matches ++;
+                    break;
+                }
+            }
+        }
+        return matches;
+    }
+
 }
